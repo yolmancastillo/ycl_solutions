5263273105cfdcc192ca463e0a5b66bfc6d94e02
Yolman Castillo <yolman.castillo@soy.sena.edu.co>
2024-01-20 20:52:19 -0500

    Se agrega el proyecto ycl_solutions

---
+++ src/main/java/com/ycl_solutions/ycl_solutions/YclSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.ycl_solutions.ycl_solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YclSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YclSolutionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ycl_solutions/ycl_solutions/YclSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycl_solutions.ycl_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YclSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
